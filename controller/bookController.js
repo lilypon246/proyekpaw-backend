99003635e4f7b491d8a5ab183c2d274690ba422a
Thoriqutsani Anastyo Rasnan <74649373+goodguythor@users.noreply.github.com>
2023-09-21 20:04:31 +0700

    feat : edit delete book

--- controller/bookController.js
+++ controller/bookController.js
@@ -15,14 +15,14 @@ const bookController = {
     // Menghapus satu buku
     deleteBook: async (req, res) => {
         try {
-            const id = req.params.bookID
-            const book = await Book.findOneAndDelete(id)
+            const id = req.params.bookID;
+            const book = await Book.findOneAndDelete({bookID: id});
             res.status(200).json(book);
         }
         catch(error) {
-            res.status(500).json({message: "Server Error", error})
+            res.status(500).json({message: "Server Error", error});
         }
-    }
+    },
 };
 
 module.exports = bookController;
