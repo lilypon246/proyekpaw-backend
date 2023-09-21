4ce55f2148263da10f720db000551a5d04560fb1
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-09-21 18:46:59 +0700

    feat : created transaction model

---
+++ models/transactionModel.js
@@ -0,0 +1,29 @@
+// Subdocument schema untuk merekam detail buku dalam transaksi
+const bookSchema = new mongoose.Schema({
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book' // Mengacu pada model 'Book'
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+
+const transactionSchema = new mongoose.Schema({
+    idTransaction: {
+        type: String,
+        required: true
+    },
+    books: [bookSchema], // Menggunakan array untuk merekam detail buku
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    employeeID: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transaction', transactionSchema);
