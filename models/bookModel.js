71ad3c4b39b71edf7895673fc4ae1013a31c03ea
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-09-21 18:45:46 +0700

    feat: created book model

---
+++ models/bookModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    bookID: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    }, 
+    genre: {
+        type: String,
+        required: true
+    }});
+
+module.exports = mongoose.model('Book', bookSchema);
