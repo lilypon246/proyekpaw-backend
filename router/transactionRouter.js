f32c0c5132bda3dc4f032c73affb931af1492c3c
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-09-22 21:57:44 +0700

    feat: add get transaction router

--- router/transactionRouter.js
+++ router/transactionRouter.js
@@ -6,5 +6,8 @@ const transactionController = require('../controller/transactionController');
 //membuat transaksi
 router.post('/', transactionController.createTransaction);
 
+//mendapatkan semua transaksi
+router.get('/', transactionController.getAllTransactions);
+
 
 module.exports = router;
