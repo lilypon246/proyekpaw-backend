4511c443419177fb09bd6d80e4781191202ab345
mailbau <simailpipiw@gmail.com>
2023-09-25 20:24:16 +0700

    feat: add feature update transaction

--- router/transactionRouter.js
+++ router/transactionRouter.js
@@ -9,7 +9,10 @@ router.post('/', transactionController.createTransaction);
 //mendapatkan semua transaksi
 router.get('/', transactionController.getAllTransactions);
 
+//update transaksi
+router.put('/:id', transactionController.updateTransaction);
+
 //menghapus transaksi
-router.delete('/:id',transactionController.deleteTransaction)
+router.delete('/:id', transactionController.deleteTransaction)
 
 module.exports = router;
